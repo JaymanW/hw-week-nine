f88af0862edfb5e933a98943d2faac861bfd188d
Jayden W <jaymanw2000@gmail.com>
2021-03-08 22:09:03 -0600

    license badge functionality

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === `MIT`) {
+    return "To learn more about this license, visit (https://choosealicense.com/licenses/mit/)";
+  } else if (license === `Apache 2.0`) {
+    return "To learn more about this license, visit (https://choosealicense.com/licenses/apache-2.0/)";
+  } else if (license === `GNU GPL 3.0`) {
+    return "To learn more about this license, visit (https://choosealicense.com/licenses/gpl-3.0/)";
+  } else if (license === `Unlicense`) {
+    return "To learn more about this license, visit (https://choosealicense.com/licenses/unlicense/)";
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
